9f00346126394524178f92c67a4b1ba2c13d2365
Lewis Vine <lew.vine@gmail.com>
2021-02-22 13:14:03 -0500

    Added two tests to Add Method Tests

---
+++ CustomListTest/AddMethodUnitTest.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class AddMethodUnitTest
+    {
+        [TestMethod]
+        public void Add_Item_CountIncreasesByOne()
+        {
+            //Arrange
+            CustomListTest<int> collectionOfNumbers = new CustomList<int>();
+            int number = 5;
+            int expected = 1;
+            int actual;
+
+            //Act
+            collectionOfNumbers.Add(number);
+            actual = collectionOfNumbers.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_Item_CountIncreasesByOneIfNull()
+        {
+            //Arrange
+            CustomListTest<int> collectionOfNumbers = new CustomList<int>();
+            int number = 5;
+            int expected = 1;
+            int actual;
+
+            //Act
+            collectionOfNumbers.Add(number);
+            actual = collectionOfNumbers.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Add_Item_IfInputIsDifferentDataTypeAsList_CountNoChange()
+        {
+            //Arrange
+            CustomListTest<int> collectionOfNumbers = new CustomList<int>();
+            string nullString = "";
+            int expected = 0;
+            int actual;
+
+            //Act
+            collectionOfNumbers.Add(nullString);
+            actual = collectionOfNumbers.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
