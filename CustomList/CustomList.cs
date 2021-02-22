bc790ff807caaa4356028ee7bb40b9db077d3e38
Lewis Vine <lew.vine@gmail.com>
2021-02-22 15:08:07 -0500

    Added some functionality to the Add Method

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -12,29 +12,43 @@ namespace CustomList
         //Member variables AKA fields
         private int count;
         private int capacity;
+        private T[] _items;
 
-        //Member variabls AKA properties
+        //Member variables AKA properties
         public int Count
         {
             get { return count; }
         }
 
-        //Member properties
 
 
         //Constructor
         public CustomList()
         {
-            this.count = 0;
-            this.capacity = 0;
+            count = 0;
+            capacity = 4;
+            _items = new T[capacity];
         }
 
 
         //Member methods
         public void Add(T item)
         {
-            count++;
-            capacity++;
+            if(count <= capacity)
+            {
+                _items[count] = item;
+                count++;
+            }
+            else
+            {
+                T[] newArray = new T[capacity * 2];
+                for(int i = 0; _items.Length <= i; i++)
+                {
+                    newArray[i] = _items[i];
+                }
+                newArray[capacity + 1] = item;
+                capacity *= 2;
+            }
         }
     }
 }
