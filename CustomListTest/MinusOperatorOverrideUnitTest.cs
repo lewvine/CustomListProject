afeee6657c8ce6d71db9273da6616e54c6449408
Lewis Vine <lew.vine@gmail.com>
2021-02-24 15:32:43 -0500

    Completed -operator tests

---
+++ CustomListTest/MinusOperatorOverrideUnitTest.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomList;
+
+namespace CustomListTest
+{
+    [TestClass]
+    public class MinusOperatorOverrideUnitTest
+    {
+        [TestMethod]
+        public void Minus_Operator_New_List_Returns_Original_When_No_Matches()
+        {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> result = new CustomList<int>();
+            one.Add(1);
+            one.Add(2);
+            one.Add(3);
+            two.Add(4);
+            two.Add(5);
+            two.Add(6);
+
+            string expected = "[ 1, 2, 3, 4, 5, 6 ]";
+            string actual;
+            //Act
+            result = one - two;
+            actual = result.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Minus_Operator_New_List_Returns_Original_When_ListTwo_Empty()
+        {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> result = new CustomList<int>();
+            one.Add(1);
+            one.Add(2);
+            one.Add(3);
+
+            string expected = "[ 1, 2, 3, 4, 5, 6 ]";
+            string actual;
+            //Act
+            result = one - two;
+            actual = result.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Minus_Operator_New_List_Deletes_Numbers_Present_In_Both_Lists()
+        {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> result = new CustomList<int>();
+            one.Add(1);
+            one.Add(2);
+            one.Add(3);
+            two.Add(1);
+            two.Add(2);
+            two.Add(6);
+
+            string expected = "[ 3 ]";
+            string actual;
+            //Act
+            result = one - two;
+            actual = result.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Minus_Operator_New_List_Deletes_Only_One_Instance_Per_Occurence()
+        {
+            //Arrange
+            CustomList<int> one = new CustomList<int>();
+            CustomList<int> two = new CustomList<int>();
+            CustomList<int> result = new CustomList<int>();
+            one.Add(1);
+            one.Add(1);
+            one.Add(3);
+            two.Add(1);
+
+            string expected = "[ 1 ]";
+            string actual;
+            //Act
+            result = one - two;
+            actual = result.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
